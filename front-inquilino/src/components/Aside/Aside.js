c9b8463d6970b592c500bdd905ca356e5fe23d18
alex <lxalonso@gmail.com>
2021-07-13 12:22:43 +0200

    anadido static en server para subida de imagenes

--- front-inquilino/src/components/Aside/Aside.js
+++ front-inquilino/src/components/Aside/Aside.js
@@ -1,7 +1,53 @@
 
 const Aside = () => {
     return (
-        <aside>Aside</aside>
+        <nav className="asideNav">
+            <ul>
+                <li>Pérfil</li>
+                    <ul>
+                        <li>Datos de acceso</li>
+                        <li>Datos personales</li>
+                    </ul>
+                <li>Inquilino</li>
+                    <ul>
+                        <li>Buscar anuncios</li>
+                        <li>Reseñas</li>
+                            <ul>
+                                <li>Historico reseñas</li>
+                                <li>Gestión de reseñas</li>
+                            </ul>
+                    </ul>
+                <li>Casero</li>
+                    <ul>
+                        <li>Gestión de anuncios</li>
+                            <ul>
+                                <li>Añadir anuncio</li>
+                                <li>Consultar anuncios</li>
+                            </ul>
+                        <li>Gestión anuncios</li>
+                        <li>Reservas</li>
+                            <ul>
+                                <li>Gestión reservas activas</li>
+                                <li>Histórico reservas y alquilers</li>
+                            </ul>
+                        <li>Gestión reseñas</li>
+                    </ul>
+                <li>Admin</li>
+                    <ul>
+                        <li>Gestión usuarios</li>
+                        <li>Gestión anuncios</li>
+                        <li>Gestión reservas</li>
+                        <li>Gestión reseñas</li>
+                    </ul>
+                <li>Facturación  y administración</li>
+                    <ul>
+                        <li>Facturación</li>
+                        <li>Anuncios</li>
+                        <li>Reservas</li>
+                        <li>Reseñas</li>
+                    </ul>
+            </ul>
+        </nav>
     )
 }
 export default Aside
